58e04648dcfb93fcfae5f59a41d4018f087fa268
Di <pdv.spb@gmail.com>
2016-08-09 17:42:21 +0300

    have done task triange area

---
+++ lesson1/triangle_area.rb
@@ -0,0 +1,9 @@
+def area base, height
+  base * height * 0.5
+end
+
+puts "Введите величину основания треугольника:"
+base = gets.chomp.to_f
+puts "Введите высоту треугольника"
+height = gets.chomp.to_f
+puts "Площадь треугольника: #{area(base, height)}"
