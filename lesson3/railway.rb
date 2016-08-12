5349fb62ded5c9b2ae33bca15921cfd9e4741a42
Di <pdv.spb@gmail.com>
2016-08-12 19:32:24 +0300

    empty classes and methods created

--- lesson3/railway.rb
+++ lesson3/railway.rb
@@ -23,4 +23,51 @@ class Route
 	def add_waypoint(station)
 	end
 
+	def delete_waypoint(station)
+	end
+
+	def list_waypoints
+	end
+
+end
+
+class Train
+
+	def initialize(number, type, wagons_num)
+	end
+
+	def accelerate(speed)
+	end
+
+	def speed
+	end
+
+	def stop
+	end
+
+	def railcars_num
+	end
+
+	def add_wagon
+	end
+
+	def remove_wagon
+	end
+
+	def route(route)
+	end
+	
+	def get_to(station)	
+	end
+
+	def last_station
+	end
+
+	def current_station
+	end
+
+	def next_station
+	end
+
+
 end
