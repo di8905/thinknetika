98be0aa4e3c10d47c99e5d4fe8f01db9d1eea360
Di <pdv.spb@gmail.com>
2016-08-12 18:12:38 +0300

    railway develop start

---
+++ lesson3/railway.rb
@@ -0,0 +1,26 @@
+class Station
+  
+  def initialize(name)
+  	@name = name
+  end
+
+  def train_arrive(train)	
+  end
+
+  def show_trains	
+  end
+
+  def train_departure(train)
+  end
+
+end
+
+class Route
+
+	def initialize(start, finish)		
+	end
+
+	def add_waypoint(station)
+	end
+
+end
