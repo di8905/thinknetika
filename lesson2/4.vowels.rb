8b90d88830d12e5aaa2e0002cee8ea0e52ad51c2
Di <pdv.spb@gmail.com>
2016-08-12 16:42:03 +0300

    vowels bug with index fixed

--- lesson2/4.vowels.rb
+++ lesson2/4.vowels.rb
@@ -2,6 +2,6 @@ ALPHABET = ("a".."z")
 VOWELS   = ["a", "e", "i", "o", "u", "y"]
 wov_hash = {}
 ALPHABET.each_with_index do |letter, i|
-  wov_hash[letter.to_sym] = i if VOWELS.include?(letter)
+  wov_hash[letter.to_sym] = i+1 if VOWELS.include?(letter)
 end #Несколько избыточно, но для тренировки с константами захотелось
 p wov_hash
