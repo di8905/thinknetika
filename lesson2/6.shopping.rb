7b901824854c6317c3347a6d2c4af9bb131bc6f0
Di <pdv.spb@gmail.com>
2016-08-12 13:16:04 +0300

    have task 6 done

---
+++ lesson2/6.shopping.rb
@@ -0,0 +1,35 @@
+class List
+
+  def initialize
+    @items = {}
+  end
+
+  def add name
+    puts "Enter price:"
+    price = gets.chomp.to_f.round(2)
+    puts "Enter quantity:"
+    quantity = gets.chomp.to_f.round(2)
+    @items[name] = {price: price, quantity: quantity}
+  end
+
+  def print
+    @items.each_pair {|name, attr| puts "#{name}: #{attr[:price]}$ #{attr[:quantity]} pcs, total position price #{attr[:price] * attr[:quantity]}$"}
+  end
+
+  def sum
+    sum = 0
+    @items.each_value {|item| sum += item[:price] * item[:quantity]}
+    sum
+  end
+
+end
+
+list = List.new
+loop do
+  puts "Enter purchase name"
+  name = gets.chomp.to_sym
+  break if name == :stop
+  list.add name
+  list.print
+  puts "Total list sum #{list.sum}"
+end
