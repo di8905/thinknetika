4fdab23253ad5ce25dcb64561159f289518f2f0e
Di <pdv.spb@gmail.com>
2016-08-22 16:11:32 +0300

    Cargo wagon methods done

--- lesson7/cargo_wagon.rb
+++ lesson7/cargo_wagon.rb
@@ -1,7 +1,21 @@
 require_relative 'wagon.rb'
 
 class CargoWagon < Wagon
-  def initialize
-    @type = :cargo
+  attr_reader :capacity, :load
+
+  def initialize(capacity)
+    @capacity = capacity
+    @type     = :cargo
+    @load     = 0
   end
+
+  def load!(val)
+    self.load += val if (self.load + val) < capacity
+  end
+
+  def available_capacity
+    self.capacity - self.load
+  end
+
+
 end
