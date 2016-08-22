bdcad2a52b824c479f7ea7f76b90e35909e5fc35
Di <pdv.spb@gmail.com>
2016-08-22 15:50:02 +0300

    Passenger wagon taken seats attr added

--- lesson7/passenger_wagon.rb
+++ lesson7/passenger_wagon.rb
@@ -1,7 +1,12 @@
 require_relative 'wagon.rb'
 
 class PassengerWagon < Wagon
-  def initialize
-    @type = :passenger
+  attr_reader :seats_quantity
+  def initialize(seats_quantity)
+    @seats_quantity = seats_quantity
+    @taken_seats    = 0
+    @type           = :passenger
   end
+
+
 end
