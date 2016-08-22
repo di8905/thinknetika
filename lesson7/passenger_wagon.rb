7dddd7e6ec715df97e34c0785d414de5132a965e
Di <pdv.spb@gmail.com>
2016-08-22 15:59:18 +0300

    Passenger wagon methods implemented

--- lesson7/passenger_wagon.rb
+++ lesson7/passenger_wagon.rb
@@ -1,11 +1,19 @@
 require_relative 'wagon.rb'
 
 class PassengerWagon < Wagon
-  attr_reader :seats_quantity
+  attr_reader :seats_quantity, :taken_seats
   def initialize(seats_quantity)
     @seats_quantity = seats_quantity
     @taken_seats    = 0
     @type           = :passenger
+  end
+
+  def take_seat
+    @taken_seats += 1 if @taken_seats < @seats_quantity
+  end
+
+  def available_seats
+    :seats_quantity - :taken_seats
   end
 
 
